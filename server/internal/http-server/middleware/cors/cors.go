960ae068ee6492e338e0790e92fb4d4c6dfa61ff
stepan41k <russia.zont@gmail.com>
2025-03-20 10:19:27 +0300

    fix cors with  /rs/cors

--- server/internal/http-server/middleware/cors/cors.go
+++ server/internal/http-server/middleware/cors/cors.go
@@ -2,33 +2,20 @@ package cors
 
 import (
 	"net/http"
+
+	"github.com/rs/cors"
 )
 
 func NewCORS(h http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
-		w.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, Authorization")
-		w.Header().Set("Access-Control-Allow-Credentials", "true")
-
-		h.ServeHTTP(w, r)
+	c := cors.New(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:5173"},
+		AllowedMethods:   []string{http.MethodPost, http.MethodDelete, http.MethodGet, http.MethodPut},
+		AllowedHeaders:   []string{"Origin", "Content-Type", "Authorization", "cache-control"},
+		AllowCredentials: true,
+		MaxAge: 120,			
 	})
+
+	h = c.Handler(h)
+		
+	return h
 }
-
-// func NewCORS(h http.Handler) http.Handler {
-// 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-// 		c := cors.New(cors.Options{
-// 			AllowedOrigins:   []string{"http://localhost:5173", "http://localhost:8040"},
-// 			AllowedMethods:   []string{http.MethodPost, http.MethodDelete, http.MethodGet, http.MethodPut},
-// 			AllowedHeaders:   []string{"Origin", "Content-Type", "Authorization", "Cache-Control"},
-// 			AllowCredentials: true,
-// 			Debug:            false,			
-// 		})
-
-// 		h = c.Handler(h)
-
-// 		h.ServeHTTP(w, r)
-
-// 	})
-// }

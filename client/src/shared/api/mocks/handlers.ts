31f32c8114db52e8b963c732fec008eb99f6ad45
sergey-frey <sergey.frey.work@gmail.com>
2025-04-29 19:11:15 +0300

    feat: add mock users search

--- client/src/shared/api/mocks/handlers.ts
+++ client/src/shared/api/mocks/handlers.ts
@@ -1,29 +1,46 @@
-import { HttpResponse, http } from "msw";
+import { faker } from "@faker-js/faker";
+import { HttpResponse } from "msw";
+import { generateRandomUser } from "./faker";
+import { MockAPI } from "./mock-api";
+
+export const mockApi = new MockAPI("http://localhost:8040/cchat");
 
 const delay = (ms: number) => new Promise((res) => setTimeout(res, ms));
 
-const profileResolver = async () => {
-  await delay(3000);
+const users = faker.helpers.multiple(generateRandomUser, { count: 10000 });
+
+mockApi.get("user/check-username", () => {
+  return HttpResponse.json({
+    isUnique: Math.random() > 0.5,
+  });
+});
+
+mockApi.get("user/search", async ({ request }) => {
+  await delay(500);
+
+  const searchParams = new URL(request.url).searchParams;
+
+  const username = searchParams.get("username");
+  const limit = searchParams.get("limit");
+  const pagination = searchParams.get("pagination");
+
+  let filteredUsers = users.filter((user) => {
+    return user.username.startsWith(username as string);
+  });
+
+  if (!username) {
+    filteredUsers = users;
+  }
+
+  if (limit && pagination) {
+    filteredUsers = filteredUsers.slice(
+      (Number(pagination) - 1) * Number(limit),
+      Number(pagination) * Number(limit),
+    );
+  }
 
   return HttpResponse.json({
-    id: "123",
-    username: "Parker2903",
-    email: "admin@gmail.com",
+    status: 200,
+    data: filteredUsers,
   });
-};
-
-const profileHandler = http.get(
-  "http://localhost:8040/cchat/user/profile",
-  profileResolver,
-);
-
-const checkUsernameHandler = http.get(
-  "http://localhost:8040/cchat/user/check-username",
-  () => {
-    return HttpResponse.json({
-      isUnique: Math.random() > 0.5,
-    });
-  },
-);
-
-export const handlers = [profileHandler, checkUsernameHandler];
+});
